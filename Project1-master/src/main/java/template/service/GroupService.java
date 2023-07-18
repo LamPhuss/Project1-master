ad05fea79791e76e6cd90368ed2539b6c179a192
LamPhuss <119296534+LamPhuss@users.noreply.github.com>
2023-07-19 01:49:10 +0700

    Delete Project1-master directory

--- Project1-master/src/main/java/template/service/GroupService.java
+++
@@ -1,9 +0,0 @@
-package template.service;
-
-import java.util.List;
-
-public interface GroupService {
-    void createGroup(String description , String displayName , String mailNickname , String ownerId , List<String> userId);
-    void createTeam(String groupId);
-    void deleteTeam(String groupId);
-}
